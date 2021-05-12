f54bc871cdc877f43b3bc1a1aa79a334ea4c0dec
BorenaK <borenakunze@gmail.com>
2021-05-12 23:24:41 +0200

    Second

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,9 @@
-urls.py
+from django.urls import path
+from . import views     #to use the home() function from views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),      #'' empty path so it's the homepage, home view from the views, name naming of the path 
+    path('about/', views.about, name='blog-about'),
+]
+
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# function that handles traffic from the homepage to the blog
+# takes in request argument
+posts = [
+	{
+		'author': 'Jane Smith',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': '11th May 2021',
+	},
+	{
+		'author': 'Orecchio Tappato',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': '12th May 2021',
+	}
+]
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
+	
+def about(request):
+	return render(request, 'blog/about.html', {'title': 'About'})
+
